fd88014f40e05e3715adcf739f467166e7076a5d
sonic11829 <104688932+sonic11829@users.noreply.github.com>
2022-09-10 11:14:28 -0400

    Add files via upload

---
+++ pac-man-project/javascript/script.js
@@ -0,0 +1,23 @@
+//make the map or maps in which pac-man and the ghosts will traverse
+
+//create a score board that displays all the points you collected while eating dots
+
+//make the pac-dots and power-dots that pac-man will eat
+
+//make the fruits that pac-man will eat that will score him more points
+
+//make pac-man and the ghosts
+
+//give pac-man function and playabillity
+
+//give the ghosts function
+
+//prevent pac-man from entering the ghost mansion
+
+//allow pac-man and the ghosts to traverse from one side of the map to the other using the gates
+
+//if using more than one map change from current map to the next once all the pac-dots and power-dots have been collected
+
+//give pac-man a set amount of lives and award a life when a set amount of points are earned
+
+//style each element, class, id, and objects accordingly
